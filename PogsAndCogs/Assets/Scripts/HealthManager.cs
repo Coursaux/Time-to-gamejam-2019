d4859cf55ed317faedc8c3065152e95daf967cae
Jerrad <corso77@live.com>
2019-11-08 19:14:16 -0700

    added health manager

---
+++ PogsAndCogs/Assets/Scripts/HealthManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    public int totalHealth;
+    public int currentHealth;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+    }
+
+    public void AddHealth(int health)
+    {
+        currentHealth += health;
+        if (currentHealth > totalHealth)
+        {
+            currentHealth = totalHealth;
+        }
+    }
+}
