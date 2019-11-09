885ee82f945042d535b83a4672fdbe4a18b59d91
Jerrad <corso77@live.com>
2019-11-08 19:06:51 -0700

    added global variables

---
+++ PogsAndCogs/Assets/Scripts/GlobalVariables.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalVariables
+{
+    public static bool hasWrench = false;
+    public static bool slowTime = false;
+    public static bool slide = false;
+}
