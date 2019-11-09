5d792c8ea67da4c67e9cf8db4d1848498e7a3e62
Jerrad <corso77@live.com>
2019-11-08 19:52:29 -0700

    camera follow

--- PogsAndCogs/Assets/Scripts/CameraController.cs
+++ PogsAndCogs/Assets/Scripts/CameraController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
+    Vector3 offset = new Vector3(0, 0, -10);
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +16,7 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector3
+            (player.transform.position.x + offset.x, player.transform.position.y + offset.y, offset.z);
     }
 }
