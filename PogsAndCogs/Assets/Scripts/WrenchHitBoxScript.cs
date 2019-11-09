0bdb8fce244a8ec71c462d7df68771fa678f7f19
Rozario B <r_beaudin@outlook.com>
2019-11-09 15:51:11 -0700

    Created a bolt and made the health manager a abstract class
    The health manager is now a abstract class meanning it can be used for more variety of situations. The bolt is now down and it does translate downward when hit so leave enough room for it to move.

--- PogsAndCogs/Assets/Scripts/WrenchHitBoxScript.cs
+++ PogsAndCogs/Assets/Scripts/WrenchHitBoxScript.cs
@@ -6,6 +6,8 @@ public class WrenchHitBoxScript : MonoBehaviour
 {
     public float lifeTime = 0.2f;
     private float spawnTime;
+
+    private int atkDamage = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +20,15 @@ public class WrenchHitBoxScript : MonoBehaviour
         if (spawnTime + lifeTime < Time.time)
         {
             Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        HealthManager hm = col.gameObject.GetComponent<HealthManager>();
+        if(hm != null)
+        {
+            hm.TakeDamage(atkDamage);
         }
     }
 }
