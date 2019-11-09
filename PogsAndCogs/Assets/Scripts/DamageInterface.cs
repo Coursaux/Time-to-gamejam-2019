8b22c83c63657fcece74723dd0d65f179d89c366
Rozario B <r_beaudin@outlook.com>
2019-11-09 13:36:10 -0700

    Chase Enemy Implemented
    The chase enemy will wait for the player to enter it's detect range from which it will begin to chase the player and try to attack them.

--- PogsAndCogs/Assets/Scripts/DamageInterface.cs
+++ PogsAndCogs/Assets/Scripts/DamageInterface.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChaseAi : MonoBehaviour
+public class DamageInterface : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
