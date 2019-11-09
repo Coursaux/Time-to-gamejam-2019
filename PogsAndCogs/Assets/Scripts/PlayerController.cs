a68c41f01ef3ae0d4a3201bc035320fffb1559f2
Harrold Paderan <harrold_paderan@yahoo.com>
2019-11-08 20:02:28 -0700

    player checkground; rework player movement; added ground tag

--- PogsAndCogs/Assets/Scripts/PlayerController.cs
+++ PogsAndCogs/Assets/Scripts/PlayerController.cs
@@ -4,48 +4,54 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Properties
+    Rigidbody2D rbody;
 
     //Movement
     public float speed;
-    public float jump;
+    public float jumpForce;
     float moveVelocity;
 
     //Grounded Vars
     bool grounded = true;
 
+    void Start () 
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
+    {
+        HandleInput();
+    }
+
+    void HandleInput () 
     {
         //Jumping
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.W))
         {
-            if (grounded)
+            if (checkGrounded())
             {
-                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jump);
+                rbody.velocity = new Vector2(rbody.velocity.x, jumpForce);
             }
         }
 
-        moveVelocity = 0;
-
         //Left Right Movement
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-        {
-            moveVelocity = -speed;
-        }
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-        {
-            moveVelocity = speed;
-        }
-
+        moveVelocity = 0;
+        moveVelocity = Input.GetAxis("Horizontal") * speed;
         GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+    }
+        
+    //Check if Grounded - ripped from DioTheHero - Harrold
+    private bool checkGrounded() {
+        float distToGround = GetComponent<Collider2D>().bounds.extents.y;
+        RaycastHit2D collision = Physics2D.Raycast(
+            transform.position, 
+            Vector2.down, 
+            distToGround + 0.1f, 
+            LayerMask.GetMask("Ground")
+        );
 
-    }
-    //Check if Grounded
-    void OnTriggerEnter2D()
-    {
-        grounded = true;
-    }
-    void OnTriggerExit2D()
-    {
-        grounded = false;
+        return (collision.collider != null);
     }
 }
