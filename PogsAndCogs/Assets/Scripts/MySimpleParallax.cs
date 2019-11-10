a1cd511c8295f0d8a2d340a51890e51f0edda95f
Harrold Paderan <harrold_paderan@yahoo.com>
2019-11-09 17:59:41 -0700

    parallax!!!!

---
+++ PogsAndCogs/Assets/Scripts/MySimpleParallax.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MySimpleParallax : MonoBehaviour
+{
+	public float paralaxSpeed;
+
+	private Transform followObject;
+	private float previousX;
+
+	void Start () {
+		// idk if i should follow player or camera
+		followObject = GameObject.FindGameObjectWithTag("Player").transform;
+		previousX = followObject.transform.position.x;
+	}
+	
+	void Update () {
+		float deltaX = followObject.transform.transform.position.x - previousX;
+		transform.position += Vector3.right * (deltaX * paralaxSpeed);
+
+		previousX = followObject.transform.position.x;
+	}
+}
